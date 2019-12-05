48e5aa821f62caabda4d2d364f303e2b79d8a317
Priya <2016bcs520@sggs.ac.in>
2019-12-05 11:14:14 +0530

    Commit from addition

---
+++ Addtion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a, b, res;
+printf("Enter two numbers\n");
+printf("Enter first number\n");
+scanf("%d", &a);
+printf("Enter second number\n");
+scanf("%d", &b);
+res=a+b;
+printf("Result====%d", res);
+return 0;
+}
